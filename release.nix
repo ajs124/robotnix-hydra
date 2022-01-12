a2b59536a59abf6d767ebab286f129a0946bf879
ajs124 <git@ajs124.de>
2022-01-13 00:06:11 +0100

    initial attempt at dynamic runcommand for signing

--- release.nix
+++ release.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }: {
+{ pkgs ? import <nixpkgs> {} }: rec {
   sunfish-grapheneOS = {
     inherit (import ./configs/sunfish-grapheneos.nix {}) releaseScript;
   };
@@ -12,9 +12,27 @@
   };
 
   runCommandHook = {
-    example = pkgs.writeScript "run-me" ''
-      #!${pkgs.runtimeShell}
-      ${pkgs.jq}/bin/jq . "$HYDRA_JSON"
+    sign-and-serve = pkgs.runCommand "sign-and-serve" {
+      extra_path = pkgs.lib.makeBinPath (with pkgs; [
+        coreutils
+        gawk
+        gnupg
+        gnutar
+        rsync
+      ]);
+      robotnix_keystore_encrypted = pkgs.fetchurl {
+        url = "https://nc.helsinki.tools/s/b9FXZqNSYMyZXbw/download/robotnix-keys.tar.gpg";
+        sha256 = "1sypdzqil7nh9xkk18285n82ka9m4zv31wzhq3w7a52vkvja7hyl";
+      };
+      release_scripts = pkgs.lib.concatMapStringsSep " " (x: x.releaseScript) [
+        sunfish-grapheneOS
+        payton
+        davinci
+      ];
+    } ''
+      substituteAll ${./sign-and-serve.sh} $out
+      chmod +x $out
+      patchShebangs $out
     '';
   };
 }
---
+++ sign-and-serve.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+PATH=@extra_path@:$PATH
+
+tmpdir="$(mktemp -d)"
+trap "rm -rf \"$tmpdir\"" EXIT
+
+pushd "$tmpdir"
+
+umask 0027
+
+cat /run/secrets/robotnix/keystore_pw |\
+    gpg --batch --yes --passphrase-fd 0 -o - --decrypt @robotnix_keystore_encrypted@ |\
+    tar x
+
+for release_script in @release_scripts@; do
+    mkdir out
+    pushd out
+    $release_script "$tmpdir/keys" || :
+    popd
+    rsync -r -e "ssh -i /run/secrets/robotnix/ssh_key" ./out/ rup@hetzner-stuff.wg:/srv/http/robotnix/
+    rm -rf out
+done
+
+popd
